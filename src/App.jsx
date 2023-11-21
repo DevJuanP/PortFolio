d658a38557e81b4e7903673483c42b8925ddbd5d
JuanP <juanpajueloch@gmail.com>
2023-11-21 00:58:05 -0500

    comit de inicio de portafolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Juan Pajuelo</h1>
+      <span>Portafolio en construcción </span>
+      <h1>🛠</h1>
+    </>
+  )
+}
+
+export default App
